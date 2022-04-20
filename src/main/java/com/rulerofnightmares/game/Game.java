8ecbed0c9a5f8acc674c5f50e32f980d63ff1ecb
Barstok <mrbarstok@gmail.com>
2022-04-20 21:24:50 +0200

    initial commit; included FXGL deps

---
+++ src/main/java/com/rulerofnightmares/game/Game.java
@@ -0,0 +1,18 @@
+package com.rulerofnightmares.game;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Game extends GameApplication {
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
